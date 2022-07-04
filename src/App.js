41fa7ec7b8388b879b08e0d300089cfb786b0fdc
akincagatay <akincagataycaliskan@gmail.com>
2022-07-04 22:06:07 +0300

    first version

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from "react";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [items, setItems] = useState([]);
+  useEffect(() => {
+    fetch("https://twemoji-api.herokuapp.com/v1/allData/category/smileys & emotion")
+        .then(res => res.json())
+        .then(
+            (result) => {
+              setIsLoaded(true);
+              setItems(result);
+            },
+        )
+  })
+  return(
+      <div>
+        <div className="topNav">
+          <div className="leftNav">
+            <label>Twemoji API</label>
+          </div>
+          <div className="rightNav">
+            <a href="">Documentation</a>
+            <a href="">Emojis</a>
+          </div>
+        </div>
+        <div className="body">
+          <div className="box">
+            {items.map(item => (
+                <div className="card">
+                  <img src={item.svg} alt="SVG as an image" width="50" height="50"/>
+                </div>
+            ))}
+          </div>
+        </div>
+      </div>
+  )
 }
 
 export default App;
